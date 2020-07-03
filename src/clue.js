abd6dc375f80e3e4344e4478b5f9ebc8cd2f5431
goncalofm90 <goncalofm90@gmail.com>
2020-07-03 14:05:45 +0100

    finishes exercise

--- src/clue.js
+++ src/clue.js
@@ -114,7 +114,7 @@ function pickMystery(){
   
 // ITERATION 3
 function revealMystery(envelope){
-  return `**\<FIRST NAME\> \<LAST NAME\> killed Mr. Boddy using the \<WEAPON\> in the \<ROOM\>!**`
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`
 }
 
 
